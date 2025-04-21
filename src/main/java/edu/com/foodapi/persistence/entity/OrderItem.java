cdc29e533bf01125eecb862de58091b7040b2779
Alexisfllv <alexis-fl-live@hotmail.com>
2025-04-20 20:51:46 -0500

    feat (entity): entidades , orederd , oderItem

---
+++ src/main/java/edu/com/foodapi/persistence/entity/OrderItem.java
@@ -0,0 +1,25 @@
+package edu.com.foodapi.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "order_item")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private long productId;
+    private Integer quantity;
+    private BigDecimal price;
+
+}
---
+++ src/main/java/edu/com/foodapi/persistence/entity/Orderd.java
@@ -0,0 +1,35 @@
+package edu.com.foodapi.persistence.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "orderd")
+public class Orderd {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderNo;
+
+    @Column(nullable = true)
+    private LocalDateTime createTime;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private List<OrderItem> items = new ArrayList<>();
+
+
+}
